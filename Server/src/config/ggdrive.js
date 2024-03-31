70575e76f0cd52e9681a36cd800a0e03d0c7cd22
Trần Tiến Hoàng <92432287+tienhoangggg@users.noreply.github.com>
2024-04-01 02:14:39 +0700

    Update ggdrive.js

--- Server/src/config/ggdrive.js
+++ Server/src/config/ggdrive.js
@@ -1,13 +1,30 @@
 const {google} = require('googleapis');
-const dotenv = require('dotenv').config();
-const OAuth2 = google.auth.OAuth2;
-const clientID = process.env.GOOGLE_CLIENT_ID;
-const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const redirectURI = process.env.GOOGLE_REDIRECT_URI;
-const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN;
-const oauth2Client = new OAuth2(clientID, clientSecret, redirectURI);
-oauth2Client.setCredentials({
-    refresh_token: REFRESH_TOKEN
+const fs = require('fs');
+
+// Load client secrets from a local file.
+const rawdata = fs.readFileSync('ggconfig.json');
+const credentials = JSON.parse(rawdata);
+
+const clientEmail = credentials.client_email;
+const privateKey = credentials.private_key;
+const clientID = credentials.client_id;
+
+const jwtClient = new google.auth.JWT(
+  clientEmail,
+  null,
+  privateKey,
+  ['https://www.googleapis.com/auth/drive'],
+  null
+);
+
+jwtClient.authorize(function (err, tokens) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("Successfully connected!");
+  }
 });
-const drive = google.drive({ version: 'v3', auth: oauth2Client });
+
+const drive = google.drive({ version: 'v3', auth: jwtClient });
 module.exports = drive
